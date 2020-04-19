6b5ae75df7f67d885bcf4f5560c4756711f86755
Tom Loughlin <tom.f.loughlin@gmail.com>
2020-04-19 20:59:21 +0200

    modal functioning

--- src/app/pages/posts/posts.component.ts
+++ src/app/pages/posts/posts.component.ts
@@ -4,8 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { MOCK_POSTS } from 'src/app/pages/posts/model/posts.mocks';
 import { Post } from 'src/app/shared/interfaces/post.interface';
 import { Validators, FormBuilder, FormGroup } from '@angular/forms';
-import { Subject, Observable, BehaviorSubject } from 'rxjs';
-import { withLatestFrom } from 'rxjs/operators';
+import { ModalDismissReasons, NgbModal, NgbModalOptions } from '@ng-bootstrap/ng-bootstrap';
 
 interface PostForm {
   title: string;
@@ -16,6 +15,8 @@ const DEFAULT_FORM_STATE: PostForm = {
   title: '',
   message: ''
 };
+
+type ErrorTypeLimits = keyof typeof DEFAULT_FORM_STATE;
 
 const FORM_LIMITS = {
   TITLE: 200,
@@ -30,16 +31,26 @@ const FORM_LIMITS = {
 export class PostsComponent implements OnInit {
 
   postForm: FormGroup;
+  closeResult: string;
   // modalRef: BsModalRef;x
   readonly editable: boolean;
   private postState: HeaderState;
   private mockPost = MOCK_POSTS;
   private editPostState: Post;
 
+  modalOptions:NgbModalOptions;
+
+  get formControls() { return this.postForm.controls; }
+
+  get isNotEdit(): boolean {
+    return this.postState != HeaderState.POST_EDIT;
+  }
+
   constructor(
     private headerStrategyService: HeaderStrategyService,
     private route: ActivatedRoute,
-    private formBuilder: FormBuilder
+    private formBuilder: FormBuilder,
+    private modalService: NgbModal
   ) {
     // Data resolver with posts service will will maitain and handles posts
     const editQuery = this.route.snapshot.queryParamMap.get('edit');
@@ -50,7 +61,10 @@ export class PostsComponent implements OnInit {
     this.editable = editQuery != null;
     this.postState = editQuery ? HeaderState.POST_EDIT : HeaderState.POST_NEW;
     this.headerStrategyService.headerStateChange(this.postState);
-
+    this.modalOptions = {
+      backdrop:'static',
+      backdropClass:'customBackdrop'
+    }
   }
 
   ngOnInit(): void {
@@ -59,11 +73,30 @@ export class PostsComponent implements OnInit {
       title: [title, [Validators.required, Validators.maxLength(FORM_LIMITS.TITLE)]],
       message: [message, [Validators.required, Validators.maxLength(FORM_LIMITS.MESSAGE)]],
     });
+    console.log(this.postForm)
   }
 
+  open(content) {
+    this.modalService.open(content, this.modalOptions).result.then((result) => {
+      alert(result)
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+ 
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return  `with: ${reason}`;
+    }
+  }
 
   savePost() {
-    // if (this.loginForm.invalid) {
+    // if (this.postForm.invalid) {
     //   return;
     // }
     // this.authService.login(this.loginForm.value.userName);
@@ -71,8 +104,16 @@ export class PostsComponent implements OnInit {
 
   }
 
-  confirmDelete() {
-    
+  errorLimitFromContext(context: ErrorTypeLimits): string {
+    const limit = (() => {
+      if (context === 'title') {
+        return FORM_LIMITS.TITLE;
+      } else if (context === 'message') {
+        return FORM_LIMITS.MESSAGE;
+      }
+    })();
+
+    return `The ${context} cannot exceed more than ${limit} characters!`;
   }
 
   private handleFormState(): PostForm {
